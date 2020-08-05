71493d09947ddc29bbab445ed8cb3c46e793f525
hariyanto17 <hariyanto17@mhs.akba.ac.id>
2020-08-05 19:14:18 +0800

    complette service user

--- routes/refreshTokens.js
+++ routes/refreshTokens.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const refreshTokensHandler = require('./handler/refresh-tokens');
 
-router.post('/', refreshTokensHandler.create)
+router.post('/', refreshTokensHandler.create);
+router.get('/', refreshTokensHandler.getToken);
 
 module.exports = router;
